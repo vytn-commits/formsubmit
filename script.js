764419839acb29732458d0459ce73ecdd8c1a1b5
vytn-commits <valerie.yt.ng@gmail.com>
2026-07-14 16:08:03 +0930

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ hamburger.classList.toggle("active");
 navMenu.classList.toggle("active");
 });
 
+new Swiper('.img-wrapper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responosive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 2
+    },
+    768: {
+        slidesPerView: 3
+    },
+  }
+});
